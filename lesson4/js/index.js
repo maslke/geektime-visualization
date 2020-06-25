a3bc2624e2c8d64de1aca20edbe9fc1804a4626f
feng.dianjun <feng.dianjun@iwhalecloud.com>
2020-06-25 23:33:33 +0800

    renju complete

--- lesson4/js/index.js
+++ lesson4/js/index.js
@@ -10,11 +10,22 @@
     const historys = [];
     const torelance = 15;
     const radius = 20;
+    const coords = [];
 
 
     function initCanvas() {
         canvas.width = beginX * 2 + size * count;
         canvas.height = beginY * 2 + size * count;
+    }
+
+    function initCoords() {
+        for (let i = 0; i <= count; i++) {
+            const coord = [];
+            for (let j = 0; j <= count; j++) {
+                coord.push('');
+            }
+            coords.push(coord);
+        }
     }
 
     function drawPad() {
@@ -28,12 +39,35 @@
             context.stroke();
             context.closePath();
         }
+
         const endY = beginY + size * count;
         for (let i = 1; i <= 13; i++) {
             context.beginPath();
             context.moveTo(beginX + size * i, beginY);
             context.lineTo(beginX + size * i, endY);
             context.stroke();
+            context.closePath();
+        }
+
+        const chars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O'];
+        for (let i = 1; i <= 15; i++) {
+            context.font = "25px '微软雅黑'";
+            context.fillStyle = 'black';
+            context.fillText(chars[i - 1], beginX + size * (i - 1), beginY + size * count + 25);
+        }
+
+        for (let i = 1; i <= 15; i++) {
+            context.font = "25px '微软雅黑'";
+            context.fillStyle = 'black';
+            context.fillText(i, beginX - 35, beginY + size * count - size * (i - 1));
+        }
+
+        const pos = [[3, 3], [7, 7], [3, 11], [11, 3], [11, 11]];
+        for (let inx = 0; inx < pos.length; inx++) {
+            context.beginPath();
+            context.fillStyle = 'black';
+            context.arc(beginX + pos[inx][0] * size, beginY + pos[inx][1] * size, 5, 0, Math.PI * 2);
+            context.fill();
             context.closePath();
         }
     }
@@ -92,9 +126,73 @@
         return false;
     }
 
+    function canWin(marks) {
+        for (let i = 0; i < marks.length; i++) {
+            for (let j = 0; j < marks[i].length; j++) {
+                if (marks[i][j] !== 'black' && marks[i][j] !== 'white') {
+                    continue;
+                }
+                if (rightWin(marks, i, j) || downWin(marks, i, j) || rightDownWin(marks, i, j)) {
+                    return [true, marks[i][j]];
+                }
+            }
+        }
+        return [false, ''];
+    }
+
+    function rightWin(marks, i, j) {
+        var mark = marks[i][j];
+        if (i + 4 > count) {
+            return false;
+        }
+        if (marks[i + 1][j] !== mark || marks[i + 2][j] !== mark || marks[i + 3][j] !== mark || marks[i + 4][j] !== mark) {
+            return false;
+        }
+        return true;
+    }
+
+    function downWin(marks, i, j) {
+        var mark = marks[i][j];
+        if (j + 4 > count) {
+            return false;
+        }
+        if (marks[i][j + 1] !== mark || marks[i][j + 2] !== mark || marks[i][j + 3] !== mark || marks[i][j + 4] !== mark) {
+            return false;
+        }
+        return true;
+    }
+
+    function rightDownWin(marks, i, j) {
+        var mark = marks[i][j];
+        if (j + 4 > count || i + 4 > count) {
+            return false;
+        }
+        if (marks[i + 1][j + 1] !== mark || marks[i + 2][j + 2] !== mark || marks[i + 3][j + 3] !== mark || marks[i + 4][j + 4] !== mark) {
+            return false;
+        }
+        return true;
+    }
+
+
+
     function bindEvent() {
 
+        canvas.onmouseout = function (event) {
+
+            if (canWin(coords)[0]) {
+                return;
+            }
+
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            restore();
+        }
+
         canvas.onmousemove = function (event) {
+
+            if (canWin(coords)[0]) {
+                return;
+            }
+
             const currentX = event.offsetX;
             const currentY = event.offsetY;
             const x = locateX(currentX);
@@ -123,6 +221,11 @@
         }
 
         canvas.onclick = function (event) {
+
+            if (canWin(coords)[0]) {
+                return;
+            }
+
             const currentX = event.offsetX;
             const currentY = event.offsetY;
             const x = locateX(currentX);
@@ -142,6 +245,13 @@
                     y: y,
                     black: black
                 });
+                
+                coords[x / size - 1][y / size - 1] = black ? 'black' : 'white';
+                const win = canWin(coords);
+                if (win[0]) {
+                    context.fillStyle ='black';
+                    context.fillText(win[1] +', win', canvas.width / 2, 40);  
+                }
             }
         }
     }
@@ -161,6 +271,7 @@
         }
         context.fill();
         context.closePath();
+        
     }
 
     function restore() {
@@ -172,6 +283,7 @@
 
 
     initCanvas();
+    initCoords();
     drawPad();
     bindEvent();
 }());
