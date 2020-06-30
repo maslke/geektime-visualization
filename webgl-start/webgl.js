cb51031cb8e293e07e9039cd65040b99f456b28a
maslke <maslke@outlook.com>
2020-06-30 18:12:32 +0800

    webgl

--- webgl-start/webgl.js
+++ webgl-start/webgl.js
@@ -1,52 +1,59 @@
 (function(){
 
     const canvas = document.querySelector('canvas');
-    const gl = canvas.getContext('webgl');
-    
-    const vertex = ` 
-        attribute vec2 position; 
-        void main() { 
-            gl_PointSize = 1.0; 
-            gl_Position = vec4(position, 1.0, 1.0); 
+    const context = canvas.getContext('webgl');
+
+    const vertex = `
+        attribute vec2 position;
+        varying vec3 color;
+        
+        void main() {
+            gl_PointSize = 1.0;
+            color = vec3(0.5 + position * 0.5, 0.0);
+            gl_Position = vec4(position * 0.2, 1.0, 1.0);
         }
     `;
-    
+
     const fragment = `
-        precision mediump float; 
-        void main() { 
-            gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0); 
-        } 
+        precision mediump float;
+        varying vec3 color;
+
+        void main() {
+            gl_FragColor = vec4(color, 1.0);
+        }
     `;
 
-    const vertexShader = gl.createShader(gl.VERTEX_SHADER);
-    gl.shaderSource(vertexShader, vertex);
-    gl.compileShader(vertexShader);
+    const vertexShader = context.createShader(context.VERTEX_SHADER);
+    context.shaderSource(vertexShader, vertex);
+    context.compileShader(vertexShader);
 
-    const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-    gl.shaderSource(fragmentShader, fragment);
-    gl.compileShader(fragmentShader);
+    const fragmentShader = context.createShader(context.FRAGMENT_SHADER);
+    context.shaderSource(fragmentShader, fragment);
+    context.compileShader(fragmentShader);
 
-
-    const program = gl.createProgram();
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
-    gl.linkProgram(program);
-    gl.useProgram(program);
+    const program = context.createProgram();
+    context.attachShader(program, vertexShader);
+    context.attachShader(program, fragmentShader);
+    context.linkProgram(program);
+    context.useProgram(program);
 
     const points = new Float32Array([
-      -1, -1,
-      0, 1,
-      1, -1,
+        0, Math.sqrt(11),
+        2, Math.sqrt(3),
+        1, 0,
+        -1, 0,
+        -2, Math.sqrt(3)
     ]);
 
-    const bufferId = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
-    gl.bufferData(gl.ARRAY_BUFFER, points, gl.STATIC_DRAW);
+    const buffer = context.createBuffer();
+    context.bindBuffer(context.ARRAY_BUFFER, buffer);
+    context.bufferData(context.ARRAY_BUFFER, points, context.STATIC_DRAW);
 
-    const vPosition = gl.getAttribLocation(program, 'position');
-    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vPosition);
+    const vPosition = context.getAttribLocation(program, 'position');
+    context.vertexAttribPointer(vPosition, 2, context.FLOAT, false, 0, 0);
+    context.enableVertexAttribArray(vPosition);
 
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.drawArrays(gl.TRIANGLES, 0, points.length / 2);
+    context.clear(context.COLOR_BUFFER_BIT);
+    context.drawArrays(context.TRIANGLE_FAN, 0, points.length / 2);
+    //context.drawArrays(context.LINE_LOOP, 0, points.length / 2);
 }());
